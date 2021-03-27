5d66475548da976d87a3b3fdbc64e6d0402fcd45
Michael Sandell <sandell.michael@gmail.com>
2021-03-27 15:01:10 -0600

    Added footer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import Footer from './Footer/Footer';
 
 function App() {
   const [joke, setJoke] = useState<any>(null);
   const [safeMode, setSafemode] = useState<boolean>(false);
 
-  let url = `https://v2.jokeapi.dev/joke/Any${safeMode?`?safe-mode`:``}`;
+  let url = `https://v2.jokeapi.dev/joke/Any${safeMode ? `?safe-mode` : ``}`;
 
   const getJoke = () => {
     axios.get(url)
-    .then(response => {
-      if (response.data.type === 'single') {
-        setJoke({ joke: response.data.joke, type: response.data.type });
-      } else {
-        setJoke({ joke: response.data.setup, punchLine: response.data.delivery, type: response.data.type });
-      }
-    }).catch(error => {
+      .then(response => {
+        if (response.data.type === 'single') {
+          setJoke({ joke: response.data.joke, type: response.data.type });
+        } else {
+          setJoke({ joke: response.data.setup, punchLine: response.data.delivery, type: response.data.type });
+        }
+      }).catch(error => {
 
-    });
+      });
   }
 
   useEffect(() => {
@@ -42,14 +43,19 @@ function App() {
           }
         </div>
       </div>
-      <div>
-      <input className='m-2' type='checkbox' checked={safeMode} onClick={()=>{setSafemode(!safeMode)}}/>
-      <span className='text-white text-lg'>Don't show me offensive jokes</span>
-      </div>
+      <div className='flex flex-col justify-between h-1/2'>
         <div>
-        
-          <button className='m-10 border border-white text-gray-900 bg-yellow-500 w-64 h-14 rounded-lg text-3xl' onClick={()=>getJoke()}>Give me another</button>
+          <input className='m-2' type='checkbox' checked={safeMode} onClick={() => { setSafemode(!safeMode) }} />
+          <span className='text-white text-lg'>Don't show me offensive jokes</span>
+          <div className='self-start'>
+            <button className='m-10 border border-white text-gray-900 bg-yellow-500 w-64 h-14 rounded-lg text-3xl' onClick={() => getJoke()}>Give me another</button>
+            <div className='text-white'>
+              [ powered by <a href='https://jokeapi.dev/'><strong>JokeAPI</strong></a> ]
+            </div>
+          </div>
         </div>
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/Footer/Footer.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement } from 'react';
+import logo from '../assets/images/s-logo.svg';
+import twitter from '../assets/images/twitter-logo-gradient.svg';
+import instagram from '../assets/images/Instagram_Glyph_Gradient_RGB.png';
+
+
+
+
+export const Footer: React.FunctionComponent = (): ReactElement => {
+    return (
+        <div className='bg-darkGray h-16 flex justify-center'>
+            <div className=' w-2/3 flex flex-row justify-between'>
+                <div className='flex flex-row items-center'>
+                    <img className='h-full' src={logo} />
+                    <div className='text-white whitespace-nowrap'>Don't steal my stuff © {new Date().getFullYear()}</div>
+                </div>
+                <div className='text-white flex flex-row p-2 justify-between items-center'>
+                    <a className='h-12 w-12 flex m-2' href='https://twitter.com/Senor_Rata'><img className='' src={twitter} /></a>
+                    <a className='h-12 w-12 flex m-2' href='https://twitter.com/Senor_Rata'><img className='' src={instagram} /></a>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
