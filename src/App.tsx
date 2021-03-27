571d7d72bd78ed6753a6c7aeeb68cc3f2ee24c92
Michael Sandell <sandell.michael@gmail.com>
2021-03-26 23:12:29 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='flex justify-center content-center'>
+        <h1 className='w-1/2 full border border-gray-500 text-5xl rounded-lg p-5 bg-gradient-to-r from-green-400 to-blue-500 m-10'>
+          AWS
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
