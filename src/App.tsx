0f0f4f99b17d15658d1de86917eea2b904552dd7
Michael Sandell <sandell.michael@gmail.com>
2021-03-27 00:55:11 -0600

    Added joke API to display a random joke.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [joke, setJoke] = useState<any>(null);
+
+  const getJoke = () => {
+    axios.get('https://v2.jokeapi.dev/joke/Any')
+    .then(response => {
+      if (response.data.type === 'single') {
+        setJoke({ joke: response.data.joke, type: response.data.type });
+      } else {
+        setJoke({ joke: response.data.setup, punchLine: response.data.delivery, type: response.data.type });
+      }
+    });
+  }
+
+  useEffect(() => {
+    getJoke();
+  }, []);
+
   return (
-    <div className="App">
-      <div className='flex justify-center content-center'>
-        <h1 className='w-1/2 full border border-gray-500 text-5xl rounded-lg p-5 bg-gradient-to-r from-green-400 to-blue-500 m-10'>
-          AWS
-        </h1>
+    <div className="App bg-gray-900 h-screen">
+      <div className='flex flex-col justify-center h-1/2'>
+        <div className='flex justify-center'>
+          <h1 className='w-1/2 full border border-white text-5xl rounded-lg p-5 bg-gradient-to-r from-green-400 to-blue-500 m-10 text-white'>
+            {joke ? joke.joke : ''}
+          </h1>
+
+        </div>
+        <div className='flex justify-center'>
+          {joke && joke.type === 'twopart' ?
+            <h1 className='w-1/2 full border border-white text-5xl rounded-lg p-5 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 m-10 text-white'>
+              {joke.punchLine}
+            </h1> : null
+          }
+        </div>
       </div>
+        <div>
+          <button className='m-10 border border-white text-gray-900 bg-yellow-500 w-64 h-14 rounded-lg text-3xl' onClick={()=>getJoke()}>Give me another</button>
+        </div>
     </div>
   );
 }
